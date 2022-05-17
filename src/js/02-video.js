583232316fe4ac72b387a2826e49f329566ff72f
IlliaBekarevych <illiabekarevych@gmail.com>
2022-05-17 13:21:48 +0300

    Виправив ДЗ

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -20,6 +20,6 @@ player.on('timeupdate', throttle(onPlay, 1000));
 
 const timePlay = localStorage.getItem('videoplayer-current-time');
 console.log(timePlay);
-if (timePlay !== '') {
+if (timePlay) {
   player.setCurrentTime(timePlay);
 }
